2a57d2cdce15a867344296fc22ca511eeb362831
jackismoe <jacksonmo@me.com>
2020-10-09 15:58:37 -0400

    Create Menu Icon

--- js/fetch.js
+++ js/fetch.js
@@ -1,6 +1,6 @@
-let fetchUsersUrl = 'http://localhost:3000/users'
-document.addEventListener('DOMContentLoaded', () => {
-  fetch(fetchUsersUrl)
-    .then(response => response.json())
-    .then(jsonResponse => render(jsonResponse))
-})
+// let fetchUsersUrl = 'http://localhost:3000/users'
+// document.addEventListener('DOMContentLoaded', () => {
+//   fetch(fetchUsersUrl)
+//     .then(response => response.json())
+//     .then(jsonResponse => render(jsonResponse))
+// })
--- js/index.js
+++ js/index.js
@@ -1,5 +1,25 @@
-let container = document.querySelector('#container')
+// let container = document.querySelector('#container')
+// const render = (obj) => {
+//   console.log(obj)
+// }
 
-const render = (obj) => {
-  console.log(obj)
+let menu = document.querySelector('.menu-container')
+let closeBtn = document.querySelector('.closeBtn')
+let body = document.querySelector('body')
+
+document.addEventListener('click', (e) => {
+  if (e.target === menu) {
+    openNav()
+  } else if (e.target === undefined) {
+    closeNav()
+  }
+})
+
+
+function closeNav() {
+  document.getElementById("mySideNav").style.width = "0";
+}
+
+function openNav() {
+  document.getElementById("mySideNav").style.width = "250px";
 }
