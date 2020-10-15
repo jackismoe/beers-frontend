22be850fb01dbce7271903bc0f3d358193e6e47b
jackismoe <jacksonmo@me.com>
2020-10-15 14:55:06 -0400

    Build random generator and array for history

--- js/index.js
+++ js/index.js
@@ -1,25 +1,22 @@
 // menu
 let menuIcons = document.querySelector('.menu-icons')
 let body = document.querySelector('body')
+let closeBtn = document.querySelector('.closeBtn')
 
 document.addEventListener('click', (e) => {
   if (e.target === menuIcons) {
     openNav()
-  } else if (e.target === undefined || e.target === body) {
+  } else if (e.target === closeBtn || e.target === body) {
     closeNav()
   }
 })
 
-document.addEventListener('DOMContentLoaded', () => {
-  let randomBtn = document.createElement('button')
-  randomBtn.innerText = 'Click Me'
-  randomBtn.className = 'button'
-  randomBtn.addEventListener('click', showRandom())
+let clickBtn = document.createElement('button')
+clickBtn.innerText = 'Click Me'
+clickBtn.className = 'button'
+clickBtn.addEventListener('click', showRandom)
 
-  body.appendChild(randomBtn)
-
-
-})
+body.appendChild(clickBtn)
 
 function closeNav() {
   document.getElementById("mySideNav").style.width = "0";
@@ -28,8 +25,21 @@ function closeNav() {
 function openNav() {
   document.getElementById("mySideNav").style.width = "250px";
 }
+
+let beerHistory = []
+
 function showRandom() {
-  console.log()
+  console.log(beerHistory)
+  let number = Math.floor((Math.random() * 500) + 1)
+  if (beerHistory.includes(number)) {
+    showRandom()
+  } else {
+    beerHistory.push(number)
+    fetch(`http://localhost:3000/beers/${number}`)
+      .then(response => response.json())
+      .then(jsonResponse => console.log(jsonResponse))
+  }
+  console.log(beerHistory)
 }
 
 function render(obj) {
